d9c8ba55232ba1d67b4bdd9242b15aed164c8ce8
Poonam Kumari <POONAMK6@birlasoft.com>
2020-05-09 18:44:21 +0530

    router

---
+++ src/About.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Aboutstyle.css';
+const About = ()=>{
+return(
+    <div className="Aboutstyle">
+   <h1>Welcome To About Page!</h1>
+   </div>
+)
+    
+}
+export default About;
---
+++ src/Contact.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Contactstyle.css';
+const Contact = ()=>{
+return(
+    <div className="Contactstyle">
+   <h1>Welcome To Contact Page!</h1>
+   </div>
+)
+    
+}
+export default Contact;
